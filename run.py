1da25fa73a76ad36187f8f068b173e6bb1c10e63
Jolene Hayes <jolenehayes@Jolenes-MacBook-Pro.local>
2015-04-18 15:02:22 -0700

    added display index and grab args from doc

--- run.py
+++ run.py
@@ -8,6 +8,13 @@ app.secret_key = os.environ['FLASK_SECRET_KEY']
 def display_form():
 	return render_template("index.html")
 
+@app.route("/get_meetups")
+def get_form_info():
+	zipcode = request.args.get("zipcode")
+	radius = ""
+
+	return render_template("index.html")
+
 
 
 if __name__ == "__main__":
